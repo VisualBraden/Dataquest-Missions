e4ba0d5061c045e4046a88b95e476b7f32867e11
VisualBraden <bradenholgate@live.com>
2019-06-03 16:07:56 +1200

    Tiny bit

--- exploratory_data_visualization/Line Charts-142.py
+++ exploratory_data_visualization/Line Charts-142.py
@@ -6,3 +6,10 @@ unrate = pd.read_csv("unrate.csv")
 unrate["DATE"] = pd.to_datetime(unrate["DATE"])
 
 print(unrate.head(12))
+
+## 6. Introduction to Matplotlib ##
+
+import matplotlib.pyplot as plt
+
+plt.plot()
+plt.show()
