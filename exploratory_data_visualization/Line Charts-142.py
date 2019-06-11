cf77f03fe836514da90f2ae564b95ce9002fb83a
VisualBraden <bradenholgate@live.com>
2019-06-11 17:43:26 +1200

    Completed first part of exploratory data module

--- exploratory_data_visualization/Line Charts-142.py
+++ exploratory_data_visualization/Line Charts-142.py
@@ -13,3 +13,23 @@ import matplotlib.pyplot as plt
 
 plt.plot()
 plt.show()
+
+## 7. Adding Data ##
+
+plt.plot(unrate["DATE"].head(12), unrate["VALUE"].head(12))
+plt.show()
+
+## 8. Fixing Axis Ticks ##
+
+plt.plot(unrate['DATE'].head(12), unrate['VALUE'].head(12))
+plt.xticks(rotation=90)
+plt.show()
+
+## 9. Adding Axis Labels And A Title ##
+
+plt.plot(unrate['DATE'].head(12), unrate['VALUE'].head(12))
+plt.xticks(rotation=90)
+plt.xlabel("Month")
+plt.ylabel("Unemployment Rate")
+plt.title("Monthly Unemployment Trends, 1948")
+plt.show()
