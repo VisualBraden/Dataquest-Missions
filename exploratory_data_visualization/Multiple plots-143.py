a53eb9016b983fe5a67133edc66e63c1885bdff0
VisualBraden <bradenholgate@live.com>
2019-06-24 15:38:29 +1200

    Completed Line plot mission

--- exploratory_data_visualization/Multiple plots-143.py
+++ exploratory_data_visualization/Multiple plots-143.py
@@ -64,3 +64,52 @@ ax4.plot(unrate[36:48]['DATE'], unrate[36:48]['VALUE'])
 ax5.plot(unrate[48:60]['DATE'], unrate[48:60]['VALUE'])
 
 plt.show()
+
+## 8. Overlaying Line Charts ##
+
+unrate['MONTH'] = unrate['DATE'].dt.month
+fig = plt.figure(figsize=(6,3))
+plt.plot(unrate[0:12]['MONTH'], unrate[0:12]['VALUE'],c='red')
+plt.plot(unrate[12:24]['MONTH'], unrate[12:24]['VALUE'],c='blue')
+plt.show()
+
+## 9. Adding More Lines ##
+
+unrate['MONTH'] = unrate['DATE'].dt.month
+fig = plt.figure(figsize=(10,6))
+plt.plot(unrate[0:12]['MONTH'], unrate[0:12]['VALUE'],c='red')
+plt.plot(unrate[12:24]['MONTH'], unrate[12:24]['VALUE'],c='blue')
+plt.plot(unrate[24:36]['MONTH'], unrate[24:36]['VALUE'], c='green')
+plt.plot(unrate[36:48]['MONTH'], unrate[36:48]['VALUE'], c='orange')
+plt.plot(unrate[48:60]['MONTH'], unrate[48:60]['VALUE'], c='black')
+plt.show()
+
+## 10. Adding A Legend ##
+
+fig = plt.figure(figsize=(10,6))
+colors = ['red', 'blue', 'green', 'orange', 'black']
+labels = ['1948', '1949', '1950', '1951', '1952']
+for i in range(5):
+    start_index = i*12
+    end_index = (i+1)*12
+    subset = unrate[start_index:end_index]
+    plt.plot(subset['MONTH'], subset['VALUE'], c=colors[i], label=labels[i])
+plt.legend(loc='upper left')    
+plt.show()
+
+## 11. Final Tweaks ##
+
+fig = plt.figure(figsize=(10,6))
+colors = ['red', 'blue', 'green', 'orange', 'black']
+for i in range(5):
+    start_index = i*12
+    end_index = (i+1)*12
+    subset = unrate[start_index:end_index]
+    label = str(1948 + i)
+    plt.plot(subset['MONTH'], subset['VALUE'], c=colors[i], label=label)
+plt.legend(loc='upper left')
+plt.title('Monthly Unemployment Trends, 1948-1952')
+plt.xlabel('Month, Integer')
+plt.ylabel('Unemployment Rate, Percent')
+
+plt.show()
