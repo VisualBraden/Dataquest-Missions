5abe21065a953e07f80aa11a0260acc855fabd67
VisualBraden <bradenholgate@live.com>
2019-07-03 13:04:15 +1200

    Coming back after a week or so of moving house. What a schlep. Managed one tiny bit of code. The brain is struggling to come back to it.

--- exploratory_data_visualization/Bar Plots And Scatter Plots-144.py
+++ exploratory_data_visualization/Bar Plots And Scatter Plots-144.py
@@ -6,3 +6,16 @@ reviews = pd.read_csv('fandango_scores.csv')
 norm_list = ['FILM', 'RT_user_norm', 'Metacritic_user_nom', 'IMDB_norm', 'Fandango_Ratingvalue', 'Fandango_Stars']
 norm_reviews = reviews.loc[:,norm_list]
 norm_reviews
+
+## 4. Creating Bars ##
+
+import matplotlib.pyplot as plt
+from numpy import arange
+num_cols = ['RT_user_norm', 'Metacritic_user_nom', 'IMDB_norm', 'Fandango_Ratingvalue', 'Fandango_Stars']
+
+bar_heights = norm_reviews[num_cols].iloc[0].values
+bar_positions = arange(5) + 0.75
+
+fig, ax = plt.subplots()
+ax.bar(bar_positions, bar_heights, 0.5)
+plt.show()
