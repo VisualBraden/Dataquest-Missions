a0fc1d812d886918919e40caba3b38da18b5fa7a
VisualBraden <bradenholgate@live.com>
2019-07-05 12:06:41 +1200

    Formatted bar plots with matplotlib

--- exploratory_data_visualization/Bar Plots And Scatter Plots-144.py
+++ exploratory_data_visualization/Bar Plots And Scatter Plots-144.py
@@ -19,3 +19,19 @@ bar_positions = arange(5) + 0.75
 fig, ax = plt.subplots()
 ax.bar(bar_positions, bar_heights, 0.5)
 plt.show()
+
+## 5. Aligning Axis Ticks And Labels ##
+
+num_cols = ['RT_user_norm', 'Metacritic_user_nom', 'IMDB_norm', 'Fandango_Ratingvalue', 'Fandango_Stars']
+bar_heights = norm_reviews[num_cols].iloc[0].values
+bar_positions = arange(5) + 0.75
+tick_positions = range(1,6)
+
+fig, ax = plt.subplots()
+ax.bar(bar_positions, bar_heights, 0.5)
+ax.set_xticks(tick_positions)
+ax.set_xticklabels(num_cols, rotation=90)
+plt.xlabel("Rating Source")
+plt.ylabel("Average Rating")
+plt.title("Average User Rating For Avengers: Age of Ultron (2015)")
+plt.show()
