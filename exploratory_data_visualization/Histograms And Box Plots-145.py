076c4d3e821976384abca79322ab915c716a6248
VisualBraden <bradenholgate@live.com>
2019-07-18 11:04:50 +1200

    Back on the horse with histograms

--- exploratory_data_visualization/Histograms And Box Plots-145.py
+++ exploratory_data_visualization/Histograms And Box Plots-145.py
@@ -3,3 +3,10 @@
 fandango_distribution = norm_reviews.loc[:, "Fandango_Ratingvalue"].value_counts().sort_index()
 imdb_distribution = norm_reviews.loc[:, "IMDB_norm"].value_counts().sort_index()
 print(fandango_distribution, imdb_distribution)
+
+## 4. Histogram In Matplotlib ##
+
+fig, ax = plt.subplots()
+
+ax.hist(norm_reviews.loc[:, "Fandango_Ratingvalue"], range=(0,5))
+plt.show()
