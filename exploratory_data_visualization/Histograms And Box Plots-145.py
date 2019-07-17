94e9cf7b19118e7da308d8f9b78e3270398eaa24
VisualBraden <bradenholgate@live.com>
2019-07-18 11:19:49 +1200

    Back on the horse with box plots

--- exploratory_data_visualization/Histograms And Box Plots-145.py
+++ exploratory_data_visualization/Histograms And Box Plots-145.py
@@ -10,3 +10,23 @@ fig, ax = plt.subplots()
 
 ax.hist(norm_reviews.loc[:, "Fandango_Ratingvalue"], range=(0,5))
 plt.show()
+
+## 5. Comparing histograms ##
+
+fig = plt.figure(figsize=(5,20))
+ax1 = fig.add_subplot(4,1,1)
+ax2 = fig.add_subplot(4,1,2)
+ax3 = fig.add_subplot(4,1,3)
+ax4 = fig.add_subplot(4,1,4)
+
+ax1.hist(norm_reviews.loc[:, "Fandango_Ratingvalue"], 20, range=(0,5))
+ax1.set(title="Distribution of Fandango Ratings", ylim=(0,50))
+
+ax2.hist(norm_reviews.loc[:, "RT_user_norm"], 20, range=(0,5))
+ax2.set(title="Distribution of Rotten Tomatoes Ratings", ylim=(0,50))
+
+ax3.hist(norm_reviews.loc[:, "Metacritic_user_nom"], 20, range=(0,5))
+ax3.set(title="Distribution of Metacritic Ratings", ylim=(0,50))
+
+ax4.hist(norm_reviews.loc[:, "IMDB_norm"], 20, range=(0,5))
+ax4.set(title="Distribution of IMDB Ratings", ylim=(0,50))
