2237ef1ec5d5b1a340b9c645c310581b61bda72c
VisualBraden <bradenholgate@live.com>
2019-08-22 09:55:33 +1200

    Finished a little mission

--- storytelling_visualization/Improving Plot Aesthetics-147.py
+++ storytelling_visualization/Improving Plot Aesthetics-147.py
@@ -48,3 +48,25 @@ ax.spines['bottom'].set_visible(False)
 ax.legend(loc='upper right')
 ax.set_title('Percentage of Biology Degrees Awarded By Gender')
 plt.show()
+
+## 8. Comparing Gender Gap Across Degree Categories ##
+
+major_cats = ['Biology', 'Computer Science', 'Engineering', 'Math and Statistics']
+fig = plt.figure(figsize=(12, 12))
+
+for sp in range(0,4):
+    ax = fig.add_subplot(2,2,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[major_cats[sp]], c='blue', label='Women')
+    ax.plot(women_degrees['Year'], 100-women_degrees[major_cats[sp]], c='green', label='Men')
+    ax.set_ylim([0,100])
+    ax.set_xlim([1968,2011])
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+    ax.set_title(major_cats[sp])
+
+# Calling pyplot.legend() here will add the legend to the last subplot that was created.
+plt.legend(loc='upper right')
+plt.show()
