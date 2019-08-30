306068a86cc6ada6bb5f6c433ce0064b60872c84
VisualBraden <bradenholgate@live.com>
2019-08-30 13:34:40 +1200

    aggregated a little data

--- data_cleaning/Data Aggregation-343.py
+++ data_cleaning/Data Aggregation-343.py
@@ -6,3 +6,13 @@ import matplotlib
 happiness2015 = pd.read_csv("World_Happiness_2015.csv")
 first_5 = happiness2015.head()
 print(happiness2015.info())
+
+
+## 3. Using Loops to Aggregate Data ##
+
+mean_happiness = {}
+regions = happiness2015['Region'].unique()
+
+for x in regions:
+    region_group = happiness2015[happiness2015['Region'] == x]
+    mean_happiness[x] = region_group['Happiness Score'].mean()
