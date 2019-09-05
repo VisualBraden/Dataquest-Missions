29bec7bb97d1ad6e055585edfc558721d3520d9d
VisualBraden <bradenholgate@live.com>
2019-09-06 10:37:30 +1200

    groupby vs. pivot tables

--- data_cleaning/Data Aggregation-343.py
+++ data_cleaning/Data Aggregation-343.py
@@ -28,3 +28,44 @@ grouped = happiness2015.groupby('Region')
 north_america = happiness2015.iloc[[4,14],:]
 na_group = grouped.get_group('North America')
 equal = north_america == na_group
+
+## 7. Common Aggregation Methods with Groupby ##
+
+grouped = happiness2015.groupby('Region')
+means = grouped.mean()
+
+## 8. Aggregating Specific Columns with Groupby ##
+
+grouped = happiness2015.groupby('Region')
+happy_grouped = grouped["Happiness Score"]
+happy_mean = happy_grouped.mean()
+
+## 9. Introduction to the Agg() Method ##
+
+import numpy as np
+grouped = happiness2015.groupby('Region')
+happy_grouped = grouped['Happiness Score']
+def dif(group):
+    return (group.max() - group.mean())
+
+happy_mean_max = happy_grouped.agg([np.mean, np.max])
+mean_max_dif = happy_grouped.agg(dif)
+
+## 10. Computing Multiple and Custom Aggregations with the Agg() Method ##
+
+happiness_means = happiness2015.groupby('Region')['Happiness Score'].mean()
+print(happiness_means)
+
+## 11. Aggregation with Pivot Tables ##
+
+pv_happiness = happiness2015.pivot_table(values='Happiness Score', index='Region', aggfunc=np.mean, margins=True)
+pv_happiness.plot(kind='barh', xlim=(0,10), title = 'Mean Happiness Scores by Region', legend=False)
+
+world_mean_happiness = happiness2015['Happiness Score'].mean()
+
+## 12. Aggregating Multiple Columns and Functions with Pivot Tables ##
+
+grouped = happiness2015.groupby("Region")['Happiness Score', 'Family']
+happy_family_stats = grouped.agg([np.min, np.max, np.mean])
+
+pv_happy_family_stats = happiness2015.pivot_table(['Happiness Score', 'Family'], 'Region', aggfunc=[np.min, np.max, np.mean], margins=True)
